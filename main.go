7ce9fb27f2cfa602899d684b86417637f8ba173e
Gerbicid <imyslivtsev@.com>
2024-10-28 16:17:45 +0300

    Second iteration

--- main.go
+++ main.go
@@ -33,6 +33,9 @@ func (t Training) distance() float64 {
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
 func (t Training) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if t.Duration == 0 {
+		return 0
+	}
 	return float64(t.Action) * t.LenStep / MInKm / t.Duration.Hours() //проверить формулу!
 }
 
@@ -169,6 +172,9 @@ type Swimming struct {
 // Это переопределенный метод Calories() из Training.
 func (s Swimming) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if s.Duration == 0 {
+		return 0
+	}
 	return (float64(s.LengthPool) * float64(s.CountPool) / MInKm / s.Duration.Hours())
 }
 
